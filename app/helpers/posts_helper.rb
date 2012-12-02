ef2f96957e961e90e0fa7b82d0a3a5ffcc01dd7c
Chris Oliver <excid3@gmail.com>
2012-12-02 12:18:05 -0600

    Add html/pipeline

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def post_body(post)
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SyntaxHighlightFilter,
+      HTML::Pipeline::AutolinkFilter
+    ]
+    pipeline.call(post.body)[:output].html_safe
+  end
 end
