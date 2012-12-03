399ad1006072ee20649a1d28bed0ed30c1aa707e
Chris Oliver <excid3@gmail.com>
2012-12-02 19:40:53 -0600

    Posts are shown most recent first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
